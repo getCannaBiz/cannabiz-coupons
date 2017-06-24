0a06d1f0882e71fa22fe5aa4ce75f4fe79b10792
Robert DeVore <deviodigital@gmail.com>
2017-06-24 02:38:00 -0400

    Adding $couponlink for widget

--- dispensary-coupons.php
+++ dispensary-coupons.php
@@ -561,6 +561,14 @@ class wpdcoupons_widget extends WP_Widget {
 			$do_not_duplicate = $post->ID;
 			
 					echo "<div class='wpd-coupons-plugin-meta'>";
+			$theme = wp_get_theme(); // gets the current theme so we can check for CannaBiz from WP Dispensary
+			if ( 'CannaBiz' == $theme->name || 'CannaBiz' == $theme->parent_theme ) {
+				$couponlink = " target='_blank'";
+			} else {
+				$couponlink = '';
+			}
+
+			echo "<div class='wpd-coupons-plugin-meta'>";
 
 					if( 'on' == $instance['couponimage'] ) {
 						/** Display coupon featured image */
