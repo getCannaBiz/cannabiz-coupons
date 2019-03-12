75ee6bd288450a412b4c7994cdfa13d7fa8e29cd
Robert DeVore <deviodigital@gmail.com>
2019-03-12 14:21:34 -0400

    :ok_hand: IMPROVE: Updated with support for Gutenberg editor in WordPress 5.0+

--- dispensary-coupons.php
+++ dispensary-coupons.php
@@ -55,15 +55,17 @@ if ( ! function_exists( 'wp_dispensary_coupons' ) ) {
 /** Register Custom Post Type */
 function wp_dispensary_coupons() {
 
+	// Get permalink base for Coupons.
 	$wpd_coupons_slug = get_option( 'wpd_coupons_slug' );
 
+	// If custom base is empty, set default.
 	if ( '' == $wpd_coupons_slug ) {
 		$wpd_coupons_slug = 'coupons';
 	}
 
-	/**
-	 * Defining variables
-	 */
+	// Capitalize first letter of new slug.
+	$wpd_coupons_slug_cap = ucfirst( $wpd_coupons_slug );
+
 	$rewrite = array(
 		'slug'       => $wpd_coupons_slug,
 		'with_front' => true,
@@ -99,25 +101,26 @@ function wp_dispensary_coupons() {
 		'filter_items_list'     => __( 'Filter coupons list', 'wpd-coupons' ),
 	);
 	$args = array(
-		'label'                 => __( 'Coupons', 'wpd-coupons' ),
-		'description'           => __( 'Display your dispensary coupons', 'wpd-coupons' ),
-		'labels'                => $labels,
-		'supports'              => array( 'title', 'thumbnail', ),
-		'taxonomies'            => array( ),
-		'hierarchical'          => false,
-		'public'                => true,
-		'show_ui'               => true,
-		'show_in_menu'          => false,
-		'menu_position'         => 10,
-		'menu_icon'             => plugin_dir_url( __FILE__ ) . ( 'images/menu-icon.png' ),
-		'show_in_admin_bar'     => true,
-		'show_in_nav_menus'     => true,
-		'can_export'            => true,
-		'has_archive'           => false,
-		'exclude_from_search'   => true,
-		'publicly_queryable'    => false,
-		'rewrite'               => $rewrite,
-		'capability_type'       => 'page',
+		'label'               => sprintf( esc_html__( '%s', 'wp-dispensary' ), $wpd_coupons_slug_cap ),
+		'description'         => sprintf( esc_html__( 'Display your %s', 'wp-dispensary' ), $wpd_coupons_slug ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'thumbnail', ),
+		'taxonomies'          => array(),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => false,
+		'show_in_rest'        => true,
+		'menu_position'       => 10,
+		'menu_icon'           => plugin_dir_url( __FILE__ ) . ( 'images/menu-icon.png' ),
+		'show_in_admin_bar'   => true,
+		'show_in_nav_menus'   => true,
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => true,
+		'publicly_queryable'  => false,
+		'rewrite'             => $rewrite,
+		'capability_type'     => 'post',
 	);
 	register_post_type( 'coupons', $args );
 
