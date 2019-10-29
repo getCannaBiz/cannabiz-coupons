80aae4bc9efb824f428da5981be41246a3a5af3c
Robert DeVore <deviodigital@gmail.com>
2019-10-29 19:07:56 -0400

    :package: NEW: coupons metabox for Gear and Tinctures menu types

--- dispensary-coupons.php
+++ dispensary-coupons.php
@@ -526,6 +526,135 @@ new Coupons_Growers();
 
 
 /**
+ * Gear Coupons
+ *
+ * Adds the option to select Gear in the Dispensary Coupons custom post type
+ *
+ * @since    1.9.0
+ */
+class Coupons_Gear {
+	var $FOR_POST_TYPE     = 'coupons';
+	var $SELECT_POST_TYPE  = 'gear';
+	var $SELECT_POST_LABEL = 'Gear';
+	var $box_id;
+	var $box_label;
+	var $field_id;
+	var $field_label;
+	var $field_name;
+	var $meta_key;
+	function __construct() {
+		add_action( 'admin_init', array( $this, 'admin_init' ) );
+	}
+	function admin_init() {
+		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
+		add_action( 'save_post', array( $this, 'save_post' ), 10, 2 );
+		$this->meta_key    = "_selected_{$this->SELECT_POST_TYPE}";
+		$this->box_id      = "select-{$this->SELECT_POST_TYPE}-metabox";
+		$this->field_id    = "selected_{$this->SELECT_POST_TYPE}";
+		$this->field_name  = "selected_{$this->SELECT_POST_TYPE}";
+		$this->box_label   = __( 'Apply Coupon to Gear', 'wpd-coupons' );
+		$this->field_label = __( "Choose {$this->SELECT_POST_LABEL}", 'wpd-coupons' );
+	}
+	function add_meta_boxes() {
+		add_meta_box(
+			$this->box_id,
+			$this->box_label,
+			array( $this, 'select_box' ),
+			$this->FOR_POST_TYPE,
+			'side'
+		);
+	}
+	function select_box( $post ) {
+		$selected_post_id = get_post_meta( $post->ID, $this->meta_key, true );
+		global $wp_post_types;
+		$save_hierarchical = $wp_post_types[ $this->SELECT_POST_TYPE ]->hierarchical;
+		$wp_post_types[ $this->SELECT_POST_TYPE ]->hierarchical = true;
+		wp_dropdown_pages( array(
+			'id'               => $this->field_id,
+			'name'             => $this->field_name,
+			'selected'         => empty( $selected_post_id ) ? 0 : $selected_post_id,
+			'post_type'        => $this->SELECT_POST_TYPE,
+			'show_option_none' => $this->field_label,
+		));
+		$wp_post_types[ $this->SELECT_POST_TYPE ]->hierarchical = $save_hierarchical;
+	}
+	function save_post( $post_id, $post ) {
+		if ( $post->post_type == $this->FOR_POST_TYPE && isset( $_POST[ $this->field_name ] ) ) {
+			$fieldgear = sanitize_text_field( $_POST['selected_gear'] );
+			update_post_meta( $post_id, $this->meta_key, $fieldgear );
+		}
+	}
+}
+new Coupons_Gear();
+
+
+/**
+ * Tinctures Coupons
+ *
+ * Adds the option to select a Tincture in the Dispensary Coupons custom post type
+ *
+ * @since    1.9.0
+ */
+class Coupons_Tinctures {
+	var $FOR_POST_TYPE     = 'coupons';
+	var $SELECT_POST_TYPE  = 'tinctures';
+	var $SELECT_POST_LABEL = 'Tinctures';
+	var $box_id;
+	var $box_label;
+	var $field_id;
+	var $field_label;
+	var $field_name;
+	var $meta_key;
+	function __construct() {
+		add_action( 'admin_init', array( $this, 'admin_init' ) );
+	}
+	function admin_init() {
+		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
+		add_action( 'save_post', array( $this, 'save_post' ), 10, 2 );
+		$this->meta_key    = "_selected_{$this->SELECT_POST_TYPE}";
+		$this->box_id      = "select-{$this->SELECT_POST_TYPE}-metabox";
+		$this->field_id    = "selected_{$this->SELECT_POST_TYPE}";
+		$this->field_name  = "selected_{$this->SELECT_POST_TYPE}";
+		$this->box_label   = __( 'Apply Coupon to Tincture', 'wpd-coupons' );
+		$this->field_label = __( "Choose {$this->SELECT_POST_LABEL}", 'wpd-coupons' );
+	}
+	function add_meta_boxes() {
+		add_meta_box(
+			$this->box_id,
+			$this->box_label,
+			array( $this, 'select_box' ),
+			$this->FOR_POST_TYPE,
+			'side'
+		);
+	}
+	function select_box( $post ) {
+		$selected_post_id = get_post_meta( $post->ID, $this->meta_key, true );
+		global $wp_post_types;
+		$save_hierarchical = $wp_post_types[ $this->SELECT_POST_TYPE ]->hierarchical;
+		$wp_post_types[ $this->SELECT_POST_TYPE ]->hierarchical = true;
+		wp_dropdown_pages( array(
+			'id'               => $this->field_id,
+			'name'             => $this->field_name,
+			'selected'         => empty( $selected_post_id ) ? 0 : $selected_post_id,
+			'post_type'        => $this->SELECT_POST_TYPE,
+			'show_option_none' => $this->field_label,
+		));
+		$wp_post_types[ $this->SELECT_POST_TYPE ]->hierarchical = $save_hierarchical;
+	}
+	function save_post( $post_id, $post ) {
+		if ( $post->post_type == $this->FOR_POST_TYPE && isset( $_POST[ $this->field_name ] ) ) {
+			$fieldtincture = sanitize_text_field( $_POST['selected_tincture'] );
+			update_post_meta( $post_id, $this->meta_key, $fieldtincture );
+		}
+	}
+}
+new Coupons_Tinctures();
+
+
+}
+
+
+/**
  * Dispensary Coupons Widget
  *
  * @since       1.0.0
